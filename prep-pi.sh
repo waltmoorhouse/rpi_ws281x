b37a21090d5ea873e6906fc0a3ef6b532b472a50
Walt Moorhouse <walt.moorhouse@gmail.com>
2019-09-07 23:50:40 -0700

    added script to prep raspberry pi for compilation.

---
+++ prep-pi.sh
@@ -0,0 +1,6 @@
+sudo apt update
+sudo apt upgrade
+sudo apt install gcc make build-essential git wiringpi
+sudo apt autoremove
+wget https://download.bell-sw.com/java/12.0.2/bellsoft-jdk12.0.2-linux-arm32-vfp-hflt.deb
+sudo apt-get install ./bellsoft-jdk12.0.2-linux-arm32-vfp-hflt.deb
